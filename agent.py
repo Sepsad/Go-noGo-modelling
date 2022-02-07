194bf91d4e633a27f496ec9d5a5a9c99bb51d12f
Sepsad <sepehrsdp@gmail.com>
2022-02-08 01:22:36 +0330

    Parameters are added

--- agent.py
+++ agent.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 import random
 
-from sympy import re
 from utils import softmax
+# from sympy import re
 
 
 class Bandit(object):
@@ -58,41 +58,68 @@ class Bandit(object):
 
 
 class Agent(object):
-    def __init__(self, bandit, beta=0.1, alpha=0.2):
+    def __init__(self, bandit, params={}):
         self.Q = {}
+        self.V = {}
         self.bandit = bandit
-        self.beta = beta
-        self.alpha = alpha
+        self.params = params
+
+        try:
+            self.alpha = params['alpha']
+            self.beta = params['beta']
+            self.Pav = params['Pav']
+            self.noise = params['noise']
+            self.bias = params['bias']
+        except:
+            print("Error in parameters")
+            sys.exit(-1)
+
         self.actions = self.bandit.actions
         self.contexts = self.bandit.get_context_list()
         self.n = len(self.actions)
-        # print(self.contexts)
+
         # init small random values to avoid ties
         for context in self.contexts:
+            self.V[context] = np.random.uniform(0, 1e-4)
             self.Q[context] = {}
             for action in self.actions:
                 self.Q[context][action] = np.random.uniform(0, 1e-4)
 
         self.log = {'context': [], 'action': [],
-                    'reward': [], 'Q(go)': [], 'Q(nogo)': []}
+                    'reward': [], 'p_go': [], 'Q(go)': [], 'Q(nogo)': []}
 
     def choose_action(self, context):
-        p = softmax(self.Q[context], self.beta)
+        Q_go = self.get_Q(context, 'go')
+        Q_nogo = self.get_Q(context, 'nogo')
+        V_state = self.get_V(context)
+
+        p = softmax([(Q_go + self.bias + self.Pav*V_state), Q_nogo]
+                    ) * (1 - self.noise) + self.noise/2
+
         action = np.random.choice(self.actions, p=p)
-        return action
+        p_go = p[0]
+
+        return action, p_go
 
     def update_action_value(self, context, action, reward):
-        error = reward - self.get_Q(context, action)
-        self.Q[context][action] = self.Q[context][action] + self.alpha * error
+        error_Q = self.beta*reward - self.get_Q(context, action)
+        self.Q[context][action] = self.Q[context][action] + \
+            self.alpha * error_Q
+
+        error_V = self.beta*reward - self.get_V(context)
+        self.V[context] = self.V[context] + self.alpha * error_V
 
     def get_Q(self, context, action):
         return self.Q[context][action]
+
+    def get_V(self, context):
+        return self.V[context]
 
     def run(self):
         contexts_sequence = self.bandit.get_random_context_list()
 
         for context in contexts_sequence:
-            action = self.choose_action(context)
+            action, p_go = self.choose_action(context)
             reward = self.bandit.reward(action, context)
 
             # update action value
@@ -102,15 +129,16 @@ class Agent(object):
             self.log['context'].append(context)
             self.log['action'].append(action)
             self.log['reward'].append(reward)
+            self.log['p_go'].append(p_go)
             self.log['Q(go)'].append(self.get_Q(context, 'go'))
             self.log['Q(nogo)'].append(self.get_Q(context, 'nogo'))
 
 
-def run_experiment(bandit, n_runs, beta, alpha):
-    print('Running a go-nogo experiment simulation with beta={}, alpha={}'.format(beta, alpha))
+def run_experiment(bandit, n_runs, params={}) -> pd.DataFrame:
+    print('Running a go-nogo experiment simulation with params = {}'.format(params))
 
     # init agent
-    agent = Agent(bandit, beta, alpha)
+    agent = Agent(bandit, params=params)
 
     for _ in range(n_runs):
         agent.run()
